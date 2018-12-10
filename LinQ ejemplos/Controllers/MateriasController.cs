2604e2b220763c5faef6170d41ed135eb8801164
unknown <SPUNIVO11@SPUNIVO11.sv.cds>
2018-12-10 10:37:11 -0600

    CambioEjemplo

---
+++ LinQ ejemplos/Controllers/MateriasController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LinQ_ejemplos.Models;
+
+namespace LinQ_ejemplos.Controllers
+{
+    public class MateriasController : Controller
+    {
+            Estudiante c = new Estudiante();
+        // GET: Materias
+        public ActionResult Index()
+        {
+
+            var listado = c.Lista();
+            return View(listado.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(string txtbuscar, string opcion, string btn)
+        {
+            var listado = c.Lista();
+           
+            var query1 = (from A in listado  select A);
+
+
+
+            switch (opcion)
+            {
+                case "nombre":
+                    var query = (from J in listado where J.Nombre.Contains(txtbuscar) select J);
+                    return View(query);
+                case "materia":
+                    var query4 = (from P in listado where P.Materia.Contains(txtbuscar) select P);
+                    return View(query4);
+                case "promedio":
+                    var query7 = (from P in listado where P.Promedio.ToString().StartsWith(txtbuscar) select P);
+                    return View(query1);
+            }
+
+            var lista = c.Lista();
+
+            var query2 = (from A in listado select A);
+
+
+
+            switch (btn)
+            {
+                case "nombres":
+                    var query = (from J in listado where J.Nombre.Contains(txtbuscar) select J);
+                    return View(query);
+                case "materias":
+                    var query4 = (from P in listado where P.Materia.Contains(txtbuscar) select P);
+                    return View(query4);
+                case "promedios":
+                    var query7 = (from P in listado where P.Promedio.ToString().StartsWith(txtbuscar) select P);
+                    return View(query7);
+            }
+
+            return View(query1.ToList());
+
+        }
+     
+    }
+}
---
+++ LinQ ejemplos/Models/Estudiante.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinQ_ejemplos.Models
+{
+    public class Estudiante
+    {
+        public int Carnet { get; set; }
+        public string Nombre { get; set; }
+        public string Materia { get; set; }
+        public decimal Promedio { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public List<Estudiante> Lista()
+        {
+            var estudiante1 = new Estudiante()
+            {
+                Carnet = 001,
+                Nombre = "Juan",
+                Materia = "Informatica",
+                Promedio = 8,
+                Fecha = DateTime.Now
+
+            };
+            var estudiante2 = new Estudiante()
+            {
+                Carnet = 002,
+                Nombre = "J. Daniel",
+                Materia = "Matematica",
+                Promedio = 7,
+                Fecha = DateTime.Now
+
+            };
+            var estudiante3 = new Estudiante()
+            {
+                Carnet = 003,
+                Nombre = "Sandra Juarez",
+                Materia = "Psicologia",
+                Promedio = 7,
+                Fecha = DateTime.Now
+
+            };
+            var estudiante4 = new Estudiante()
+            {
+                Carnet = 004,
+                Nombre = "Alexis",
+                Materia = "Psicologia",
+                Promedio = 8,
+                Fecha = DateTime.Now
+
+            };
+            return new List<Estudiante>() {estudiante1, estudiante2, estudiante3, estudiante4 };
+    }
+    }
+     
+}
